65c2de4409d37459c2e2bf323feafc7ad9e5cafa
Sulabasana <piotr.frydryszak@gmail.com>
2020-06-02 11:06:25 +0200

    Functionality fixed

--- PDFbreaker/pdfopen.py
+++ PDFbreaker/pdfopen.py
@@ -2,7 +2,7 @@ import pikepdf
 from tqdm import tqdm
 
 # load password list
-passwords = [ line.strip() for line in open("wordlist.txt") ]
+passwords = [ line.strip() for line in open("wordlist.txt", encoding="utf-8") ]
 
 # iterate over passwords
 for password in tqdm(passwords, "Decrypting PDF"):
