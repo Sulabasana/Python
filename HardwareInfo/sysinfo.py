6646137b994d9dc5a9cec58bc853173fd4631155
Sulabasana <piotr.frydryszak@gmail.com>
2020-06-06 18:29:04 +0200

    Completed

--- HardwareInfo/sysinfo.py
+++ HardwareInfo/sysinfo.py
@@ -50,6 +50,7 @@ print(f"Total CPU Usage: {psutil.cpu_percent()}%")
 
 # Memory Information
 print("="*40, "Memory Information", "="*40)
+
 # get the memory details
 svmem = psutil.virtual_memory()
 print(f"Total: {get_size(svmem.total)}")
@@ -57,9 +58,62 @@ print(f"Available: {get_size(svmem.available)}")
 print(f"Used: {get_size(svmem.used)}")
 print(f"Percentage: {svmem.percent}%")
 print("="*20, "SWAP", "="*20)
+
 # get the swap memory details (if exists)
 swap = psutil.swap_memory()
 print(f"Total: {get_size(swap.total)}")
 print(f"Free: {get_size(swap.free)}")
 print(f"Used: {get_size(swap.used)}")
 print(f"Percentage: {swap.percent}%")
+
+# Disk Information
+print("="*40, "Disk Information", "="*40)
+print("Partitions and Usage:")
+
+# get all disk partitions
+partitions = psutil.disk_partitions()
+for partition in partitions:
+    print(f"=== Device: {partition.device} ===")
+    print(f"  Mountpoint: {partition.mountpoint}")
+    print(f"  File system type: {partition.fstype}")
+    try:
+        partition_usage = psutil.disk_usage(partition.mountpoint)
+    except PermissionError:
+        # this can be catched due to the disk that
+        # isn't ready
+        continue
+    print(f"  Total Size: {get_size(partition_usage.total)}")
+    print(f"  Used: {get_size(partition_usage.used)}")
+    print(f"  Free: {get_size(partition_usage.free)}")
+    print(f"  Percentage: {partition_usage.percent}%")
+
+# get IO statistics since boot
+disk_io = psutil.disk_io_counters()
+print(f"Total read: {get_size(disk_io.read_bytes)}")
+print(f"Total write: {get_size(disk_io.write_bytes)}")
+
+# Network information
+print("="*40, "Network Information", "="*40)
+# get all network interfaces (virtual and physical)
+if_addrs = psutil.net_if_addrs()
+for interface_name, interface_addresses in if_addrs.items():
+    for address in interface_addresses:
+        print(f"=== Interface: {interface_name} ===")
+        if str(address.family) == 'AddressFamily.AF_INET':
+            print(f"  IP Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast IP: {address.broadcast}")
+        elif str(address.family) == 'AddressFamily.AF_PACKET':
+            print(f"  MAC Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast MAC: {address.broadcast}")
+# get IO statistics since boot
+net_io = psutil.net_io_counters()
+print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
+print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
+
+
+# Battery for laptop
+print("="*40, "Battery Information", "="*40)
+battery = psutil.sensors_battery()
+print(f" Battery: {battery.percent, battery.secsleft}")
