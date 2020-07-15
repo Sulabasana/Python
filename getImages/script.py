2a050e9250917207e33fd1e3987d2ac6c7e82368
Sulabasana <piotr.frydryszak@gmail.com>
2020-07-15 21:32:46 +0200

    Images download app done

--- getImages/script.py
+++ getImages/script.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin, urlparse
 
+
 def is_valid(url):
     """
     Checks whether `url` is a valid URL.
@@ -11,7 +12,6 @@ def is_valid(url):
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
 
-    #urlparse() function parses a URL into six components, we just need to see if the netloc (domain name) and scheme (protocl) are there.
 
 def get_all_images(url):
     """
@@ -24,18 +24,21 @@ def get_all_images(url):
         if not img_url:
             # if img does not contain src attribute, just skip
             continue
-    # make the URL absolute by joining domain with the URL that is just extracted
-    img_url = urljoin(url, img_url)
-    try:
+        # make the URL absolute by joining domain with the URL that is just extracted
+        img_url = urljoin(url, img_url)
+        # remove URLs like '/hsts-pixel.gif?c=3.2.5'
+        try:
             pos = img_url.index("?")
             img_url = img_url[:pos]
-    except ValueError:
+        except ValueError:
             pass
-    # finally, if the url is valid
-     if is_valid(img_url):
-        urls.append(img_url)
+        # finally, if the url is valid
+        if is_valid(img_url):
+            urls.append(img_url)
     return urls
-def download (url, pathname):
+
+
+def download(url, pathname):
     """
     Downloads a file given an URL and puts it in the folder `pathname`
     """
@@ -44,6 +47,7 @@ def download (url, pathname):
         os.makedirs(pathname)
     # download the body of response by chunk, not immediately
     response = requests.get(url, stream=True)
+
     # get the total file size
     file_size = int(response.headers.get("Content-Length", 0))
 
@@ -58,3 +62,29 @@ def download (url, pathname):
             f.write(data)
             # update the progress bar manually
             progress.update(len(data))
+
+
+def main(url, path):
+    # get all images
+    imgs = get_all_images(url)
+    for img in imgs:
+        # for each img, download it
+        download(img, path)
+    
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="This script downloads all images from a web page")
+    parser.add_argument("url", help="The URL of the web page you want to download images")
+    parser.add_argument("-p", "--path", help="The Directory you want to store your images, default is the domain of URL passed")
+    
+    args = parser.parse_args()
+    url = args.url
+    path = args.path
+
+    if not path:
+        # if path isn't specified, use the domain name of that url as the folder name
+        path = urlparse(url).netloc
+    
+    main(url, path)
