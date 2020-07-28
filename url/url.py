2ee7767a7bf6e00ce8e137185a34f0c2698d7137
Sulabasana <piotr.frydryszak@gmail.com>
2020-07-28 06:56:47 +0200

    credentials and status check added in URL

---
+++ url/url.py
@@ -0,0 +1,15 @@
+import requests
+
+#account data
+username = ""
+password = ""
+
+# get the access token
+auth_res = requests.post("https://api-ssl.bitly.com/oauth/access_token", auth=(username, password))
+if auth_res.status_code == 200:
+    # if response is OK, get the access token
+    access_token = auth_res.content.decode()
+    print("[!] Got access token:", access_token)
+else:
+    print("[!] Cannot get access token, exiting...")
+    exit()
