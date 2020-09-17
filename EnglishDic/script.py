9e2321a699c9faefdb5df443cd834718c8559cb4
Sulabasana <piotr.frydryszak@gmail.com>
2020-09-17 20:05:54 +0200

    Application for EnglishDic

--- EnglishDic/script.py
+++ EnglishDic/script.py
@@ -6,9 +6,29 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn== "N":
+            return "The word does not exist. Please double check it"
+        else:
+            return "We did not understand your entry."
     else:
-        return "Sorry the worde does not exist. Please check it"
+        return "Sorry the word does not exist. Please double check it"
 
 word = input("Enter word: ")
 
-print(translate(word))
+# print(translate(word))
+# more user friendly
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
