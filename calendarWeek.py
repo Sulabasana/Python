35657af3b9938b01229aa37ed79a1423d9ea74be
Piotr Frydryszak <piotr.frydryszak@gmail.com>
2021-12-30 17:39:58 +0100

    CHange date format
    Change date format

--- calendarWeek.py
+++ calendarWeek.py
@@ -2,7 +2,7 @@ from datetime import date
 from datetime import datetime
 
 
-today = date.today()
+today = date.today().strftime('%d.%m.%Y')
 print (f"Today's date is", today)
 week = datetime.today().strftime("%V")
 print("Week number is " + week)
