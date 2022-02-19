83fb7ab20fe4c91c7c3d367bad8207459f97259d
Sulabasana <piotr.frydryszak@gmail.com>
2022-02-19 18:40:40 +0100

    Next steps in SQL

--- SQL/newsql.py
+++ SQL/newsql.py
@@ -18,3 +18,20 @@ cursor = db_connection.cursor()
 cursor.execute("select database();")
 database_name = cursor.fetchone()
 print("[+] You are connected to the database:", database_name)
+
+# create a new database called library
+cursor.execute("create database if not exists library")
+
+# use that database 
+cursor.execute("use library")
+print("[+] Changed to `library` database")
+
+# create a table
+cursor.execute("""create table if not exists book (
+    `id` integer primary key auto_increment not null,
+    `name` varchar(255) not null,
+    `author` varchar(255) not null,
+    `price` float not null,
+    `url` varchar(255)
+    )""")
+print("[+] Table `book` created")
