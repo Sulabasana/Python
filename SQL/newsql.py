20bdbdbfe6c49afcbc369c93930b032e5f08ab31
Sulabasana <piotr.frydryszak@gmail.com>
2022-02-20 11:29:58 +0100

    Added books

--- SQL/newsql.py
+++ SQL/newsql.py
@@ -35,3 +35,37 @@ cursor.execute("""create table if not exists book (
     `url` varchar(255)
     )""")
 print("[+] Table `book` created")
+
+# insert some books
+books = [
+    {
+        "name": "Automate the Boring Stuff with Python: Practical Programming for Total Beginners",
+        "author": "Al Sweigart",
+        "price": 17.76,
+        "url": "https://amzn.to/2YAncdY"
+    },
+    {
+        "name": "Python Crash Course: A Hands-On, Project-Based Introduction to Programming",
+        "author": "Eric Matthes",
+        "price": 22.97,
+        "url": "https://amzn.to/2yQfQZl"
+    },
+    {
+        "name": "MySQL for Python",
+        "author": "Albert Lukaszewski",
+        "price": 49.99,
+    }
+]
+# iterate over books list
+for book in books:
+    id = book.get("id")
+    name = book.get("name")
+    author = book.get("author")
+    price = book.get("price")
+    url = book.get("url")
+    # insert each book as a row in MySQL
+    cursor.execute("""insert into book (id, name, author, price, url) values (
+        %s, %s, %s, %s, %s
+    )
+    """, params=(id, name, author, price, url))
+    print(f"[+] Inserted the book: {name}")
