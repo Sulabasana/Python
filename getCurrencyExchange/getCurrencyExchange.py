406c1366429a866b1d5b472ff03a0c36f5ac8dfc
Sulabasana <piotr.frydryszak@gmail.com>
2022-09-06 17:35:09 +0200

    Added another functionality

--- getCurrencyExchange/getCurrencyExchange.py
+++ getCurrencyExchange/getCurrencyExchange.py
@@ -1,5 +1,19 @@
+# from forex_python.converter import CurrencyRates
+
+# c = CurrencyRates()
+
+# print(c.get_rate('EUR', 'PLN'))
 from forex_python.converter import CurrencyRates
 
 c = CurrencyRates()
 
-print(c.get_rate('EUR', 'PLN'))
+firstCur=input("Provide first currency: ")
+secondCur=input("Provide second currency: ")
+
+firstCur =firstCur.upper()
+secondCur=secondCur.upper()
+
+# print(firstCur)
+# print(secondCur)
+print(c.get_rate(firstCur, secondCur))
+
