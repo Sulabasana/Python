c1ec76fcb6d78307af9c1abcc0280a5bfdf58453
Sulabasana <piotr.frydryszak@gmail.com>
2022-09-17 13:38:34 +0200

    New idea:

--- getCurrencyExchange/readCSV.py
+++ getCurrencyExchange/readCSV.py
@@ -1,6 +1,14 @@
-import csv
-with open('file.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count = 0
-    for row in csv_reader:
-        print(row)
+# import csv
+# with open('rate.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=';')
+#     line_count = 0
+#     for row in csv_reader:
+#         addValue = row
+        
+#         print(row)
+import pandas as pd
+
+data = pd.read_csv('rate.csv')
+# print(data)
+# print(data.columns)
+print(data.Rate)
--- getCurrencyExchange/readFile.py
+++ getCurrencyExchange/readFile.py
@@ -1,5 +1,9 @@
 with open('file.txt') as f:
-    reader = f.readline()
+    reader = f.readlines()
     line_count = 0
+    # print(reader)
     for row in reader:
         print(row)
+        word = "02"
+        if row.find(word) == 0:
+            print("Found")
