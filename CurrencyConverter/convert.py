b5f0064abbc80e1a2ac6242b16f0be6e9669a251
Sulabasana <piotr.frydryszak@gmail.com>
2022-10-14 18:40:38 +0200

    Pliki

--- CurrencyConverter/convert.py
+++ CurrencyConverter/convert.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup as bs
 import re
 from dateutil.parser import parse
 
-"Function that accepts the source currency, target currency, and the amount we want to convert, and then returns the converted amount"
 def convert_currency_xe(src, dst, amount):
     def get_digits(text):
         """Returns the digits and dots only from an input `text` as a float
@@ -23,7 +22,8 @@ def convert_currency_xe(src, dst, amount):
     # get the last updated datetime
     last_updated_datetime = parse(re.search(r"Last updated (.+)", exchange_rate_html.parent.parent.find_all("div")[-2].text).group()[12:])
     return last_updated_datetime, get_digits(exchange_rate_html.text)
-
+    
+    
 if __name__ == "__main__":
     import sys
     source_currency = sys.argv[1]
@@ -32,5 +32,4 @@ if __name__ == "__main__":
     last_updated_datetime, exchange_rate = convert_currency_xe(source_currency, destination_currency, amount)
     print("Last updated datetime:", last_updated_datetime)
     print(f"{amount} {source_currency} = {exchange_rate} {destination_currency}")
-
     
