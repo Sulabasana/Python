21c8549a00259c8c57cc0cb171387248ae70866c
Sulabasana <piotr.frydryszak@gmail.com>
2022-10-19 19:18:53 +0200

    Fixed

--- getCurrencyExchange/newExchange.py
+++ getCurrencyExchange/newExchange.py
@@ -2,12 +2,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
 #Get input from user
-firstCur=input("Provide first currency: ")
+firstCur = input("Provide first currency: ")
 # secondCur=input("Provide second currency: ")
-ending = "string"
-firstCur =firstCur.upper()
-# secondCur=secondCur.upper()
-# print(firstCur)
+currencyEnd = ""
+firstCur = firstCur.upper()
 
 #Check if currency is in allowed list
 if (firstCur == "EUR") or (firstCur == "USD") or (firstCur == "CHF") or (firstCur == "AUD") or (firstCur == "GBP"):
@@ -17,31 +15,38 @@ else:
   
 # Preparing URL based on currency
 def prepareURL(currency):
-    if(currency == "AUD") :
-        ending == "AUD-DOLAR-AUSTRALIJSKI"
+    # currencyEnd 
+    if(currency == "AUD"):
+        return("https://www.biznesradar.pl/waluty/nbp/AUD-DOLAR-AUSTRALIJSKI")
+    elif(currency == "USD"):
+        return("https://www.biznesradar.pl/waluty/nbp/USD-DOLAR")
+    elif(currency == "GBP"):
+        return("https://www.biznesradar.pl/waluty/nbp/GBP-FUNT-SZTERLING")
+    elif(currency == "CHF"):
+        return("https://www.biznesradar.pl/waluty/nbp/CHF-FRANK-SZWAJCARSKI")
+    elif(currency == "EUR"):
+        return("https://www.biznesradar.pl/waluty/nbp/EURO")
+       
 
-    url = f'https://www.biznesradar.pl/waluty/nbp/{ending}'
-    print(url)
-    return url
-
-prepareURL(firstCur)
+print(prepareURL(firstCur))
+url = prepareURL(firstCur)
+# print(prepareURL(firstCur))
 # Open page
-# def openPage(currency):
-#     url="https://www.biznesradar.pl/waluty/nbp/AUD-DOLAR-AUSTRALIJSKI"
-#     print(url)
-#     def scraper():
-#         response = requests.get(url)
-#         print(response)
-#         # soup = bs(response.content, 'html.parser')
-#         # rev_span = soup.findAll("span",attrs={"class","q_ch_act"})
-#         # for j in range(len(rev_span)):
-# 		# 	# finding all the p tags to fetch only the review text
-# 		#     pagewise_reviews.append(rev_div[j].find("p").text)
-#         # return all_pages_reviews
-#     reviews = scraper()
-#     print(reviews)
+def openPage(ending):
+    print(url)
+    # def scraper():
+    #     response = requests.get(url)
+    #     print(response)
+    #     soup = bs(response.content, 'html.parser')
+    #     rev_span = soup.findAll("span",attrs={"class","q_ch_act"})
+    #     for j in range(len(rev_span)):
+	# 		finding all the p tags to fetch only the review text
+	# 	    pagewise_reviews.append(rev_div[j].find("p").text)
+    #     return all_pages_reviews
+    # reviews = scraper()
+    # print(reviews)
 
-# openPage(firstCur)
+openPage(url)
 
 
 
