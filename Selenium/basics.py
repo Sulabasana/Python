f513f4f60bbb7903de0a0c8b483d28e442a87e26
Sulabasana <piotr.frydryszak@gmail.com>
2022-10-26 19:26:31 +0200

    Selenium

--- Selenium/basics.py
+++ Selenium/basics.py
@@ -1,21 +1,51 @@
-#importing required library
+# #importing required library
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
-#opening a webpage
+# #opening a webpage
 browser = webdriver.Chrome(r"C:\Users\piotr\Selenium\chromedriver_win32\chromedriver.exe")
-# Simply just open a new Edge browser and go to lambdatest.com
-# browser.maximize_window()
-# browser.get('https://www.puralsight.com/')
+browser.get('https://python.org')
 
-#closing the current window
-# browser.quit()
+search = browser.find_element_by_name('q')
+search.clear()
+search.send_keys("pycon")
+search.send_keys(Keys.RETURN)
+time.sleep(4)
 
-#opening web page with options
-options = webdriver.ChromeOptions()
-options.add_argument("--ignore-certificate-errors")
-options.add_argument("--incognito")
-options.add_argument("--headless")
-browser = webdriver.Chrome(options=options)
+browser.close()
+# element_id = browser.find_element_by_id('gsc-iw-id1')
+# print(element_id)
+# element_name = browser.find_element_by_name('submit')
+# print(element_name)
 
-browser.get("https://www.puralsight.com/")
+# element_xpath = browser.find_element_by_xpath("//section[@class='hero homepage']/h1[1]")
+# print(element_xpath)
+
+# element_classname = browser.find_element_by_class_name('selenium-backers')
+# print(element_classname)
+# # heading_xpath = browser.find_element_by_xpath("//*[@id='mainContent']/h2[1]")
+# # print(heading_xpath)
+
+# # element_classname = browser.find_element_by_class_name('selenium-sponsors')
+# # print(element_classname)
+
+# browser.close()
+# # driver.close()
+
+# # Simply just open a new Edge browser and go to lambdatest.com
+# # browser.maximize_window()
+# # browser.get('https://www.puralsight.com/')
+
+# #closing the current window
+# # browser.quit()
+
+# #opening web page with options
+# # options = webdriver.ChromeOptions()
+# # options.add_argument("--ignore-certificate-errors")
+# # options.add_argument("--incognito")
+# # options.add_argument("--headless")
+# # browser = webdriver.Chrome(options=options)
+
+# # browser.get("https://www.puralsight.com/")
 
