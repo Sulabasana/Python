bc6891ea45f2a8e3f84df8847794ab2f1f3b8893
Sulabasana <piotr.frydryszak@gmail.com>
2023-05-04 21:03:17 +0200

    reverse shell started

---
+++ reverseShell/main.py
@@ -0,0 +1,12 @@
+import socket
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5003
+BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
+# separator string for sending 2 messages in one go
+SEPARATOR = "<sep>"
+# create a socket object
+s = socket.socket()
+
+# bind the socket to all IP addresses of this host
+s.bind((SERVER_HOST, SERVER_PORT))
