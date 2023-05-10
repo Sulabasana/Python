6fbdabb1d117f36c5cc572d20fe37d434bc912c1
Piotr Frydryszak <piotr.frydryszak@gmail.com>
2023-05-10 12:55:18 +0200

    Update onet.bat

--- robot/amazon/onet.bat
+++ robot/amazon/onet.bat
@@ -1,3 +1,7 @@
 @echo off
 cd c:\Users\frydrysz\TestRobot
-call python -m robot results/LogsfromChrome Tests/Onet.robot
+call python -m robot Tests/Onet.robot
+#Change logs directory
+call python -m robot -d results/LogsfromChrome Tests/Onet.robot
+#Select browser
+call python -m robot -d results/LogsfromChrome -v BROWSER:chrome Tests/Onet.robot
