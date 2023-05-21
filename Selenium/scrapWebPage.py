7b16986d1f9097227fe41649c91ca9a9f7fee19b
Sulabasana <piotr.frydryszak@gmail.com>
2023-05-21 20:29:22 +0200

    Script almost completed

--- Selenium/scrapWebPage.py
+++ Selenium/scrapWebPage.py
@@ -19,6 +19,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
+
+# variable
+playerName = "Wayne Rooney"
+
 # adding options on chrome
 chromeOptions = Options()
 # chromeOptions.add_argument("--kiosk")
@@ -27,23 +31,58 @@ chromeOptions = Options()
 # navigating to the premier league site
 
 driver = webdriver.Chrome(options=chromeOptions)
-driver.get("https://www.premierleague.com/")
+# driver.get("https://www.premierleague.com/")
+driver.get("https://www.premierleague.com/players/")
 
 # clicking on the players tab
-players_ele = driver.find_element_by_link_text("Players").click()
+# players_ele = driver.find_element_by_link_text("Players").click()
 
-# searching for wayne rooney
+# searching for player
 # web driver wait
 element = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, "search-input")))
 
 search_ele = driver.find_element_by_id("search-input")
-search_ele.send_keys("Wayne Rooney")
+search_ele.send_keys(playerName)
 search_ele.send_keys(Keys.RETURN)
 
-# clicking on wayne rooney
+# clicking on player
 driver.implicitly_wait(3)
-click_wayne = driver.find_element_by_xpath("//img[@data-player='p13017']").click()
+playerLink =f'[alt= "Photo for " + {playerName}]'
+print(playerLink)
+# click_player = driver.find_element_by_xpath("//img[@data-player='p13017']").click()
+click_player=driver.find_elements_by_css_selector(playerLink)
+ 
 
-#Assign to variable
+# Assign to variable
 page_source_overview = driver.page_source
+# Beautiful Soup
+from bs4 import BeautifulSoup
+
+# loading page source info
+soup = BeautifulSoup(page_source_overview, 'lxml')
+
+# locating the titles
+title_finder = soup.find_all("span", class_="title")
+
+print(10*"-" + "These are the latest news headlines about " + playerName + 10*"-" + "\n")
+for title in title_finder:
+    print(title.string)
+
+# stats button element
+time.sleep(2)
+wayne_stats = driver.find_element_by_xpath("//a[@href='stats']").click()
+
+page_source_stats = driver.page_source
+
+soup = BeautifulSoup(page_source_stats, "lxml")
+
+# locating all the stats
+stat_finder = soup.find_all("span", class_="allStatContainer")
+
+print(stat_finder)
+
+print(10*"-" + playerName + " Stats" + 10*"-" + "\n")
+for stat in stat_finder:
+    print(stat["data-stat"] +  " - " + stat.string)
+
