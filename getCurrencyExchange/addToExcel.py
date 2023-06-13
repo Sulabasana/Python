6b9548dc869e985f6ac7086a3db5126f07f8dc5d
Sulabasana <piotr.frydryszak@gmail.com>
2023-06-13 19:54:12 +0200

    App updated

--- getCurrencyExchange/addToExcel.py
+++ getCurrencyExchange/addToExcel.py
@@ -1,12 +1,10 @@
 # import pandas
 import pandas as pd
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+from openFile import fileName
+# from openFile import quitApp
 
 
-fileCur = 'EURRate.txt'
+fileCur = fileName
 dateList = []
 valueList = []
 curList = []
@@ -19,7 +17,8 @@ firstChars = fileCur[0:3]
 
 print('First 3 character : ', firstChars)
 
-
+excelName = firstChars + ".xlsx"
+# print(excelName)
 # here ; and / are our two markers 
 # in which string can be found.
 # Podziel na Daty i wartości 
@@ -57,26 +56,30 @@ for x in range(rowCount):
 df = pd.DataFrame({'Name': curList,'Date': dateList,'Value': curValue})
 
 # # create a Pandas Excel writer object using XlsxWriter as the engine
-writer = pd.ExcelWriter('demo.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter(excelName, engine='xlsxwriter')
 
 # # write data to the excel sheet
 df.to_excel(writer, sheet_name=firstChars, index=False)
 
-# get workbook
-workbook = writer.book
-
-# get sheet for conditional formatting
-worksheet = writer.sheets[firstChars]
-
-# create chart object
-chart = workbook.add_chart({'type': 'line'})
-
-# configure the series of the chart from the dataframe data
-#przerobić trzeba  kropke na przecinek
-chart.add_series({'values': '=EUR!$C$2:$C$50'})
-
-# insert chart into the worksheet
-worksheet.insert_chart('E2', chart)
-
 # # close file
 writer.close()
+
+#=====================POTEM DODAĆ===========================
+# # get workbook
+# workbook = writer.book
+
+# # get sheet for conditional formatting
+# worksheet = writer.sheets[firstChars]
+
+# # create chart object
+# chart = workbook.add_chart({'type': 'line'})
+
+# # configure the series of the chart from the dataframe data
+# #przerobić trzeba  kropke na przecinek
+# chart.add_series({'values': '=EUR!$C$2:$C$50'})
+
+# # insert chart into the worksheet
+# worksheet.insert_chart('E2', chart)
+
+# # close file
+# writer.close()
---
+++ getCurrencyExchange/openFile.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+import os
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+# create the root window
+root = tk.Tk()
+root.title('Select file')
+root.resizable(False, False)
+root.geometry('300x150')
+
+
+def select_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir=r'C:\Users\piotr\Github\Python\Python\getCurrencyExchange',
+        filetypes=filetypes)
+    # showinfo(
+    #     title='Selected File',
+    #     message=filename.split('/')[len(filename.split('/'))-1]
+    # )
+    fileNext = filename.split('/')[len(filename.split('/'))-1]
+    return fileNext
+    root.quit()
+
+# quitApp = quitWind()
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open a File',
+    command=select_file,
+)
+fileName = select_file()
+
+open_button.pack(expand=True)
+
+
+# run the application
+root.mainloop()
