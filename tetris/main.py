5ba12b88d0db92314be64162f531a477b295c772
Sulabasana <piotr.frydryszak@gmail.com>
2024-04-24 22:01:12 +0200

    Tetris

---
+++ tetris/main.py
@@ -0,0 +1,110 @@
+import sys
+import pygame
+import random
+
+
+pygame.init()
+
+
+# Screen dimensions
+WIDTH, HEIGHT = 800, 600
+GRID_SIZE = 25
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+COLORS = [RED, BLUE, GREEN]
+
+# Tetromino shapes
+SHAPES = [
+    [
+        ['.....',
+         '.....',
+         '.....',
+         'OOOO.',
+         '.....'],
+        ['.....',
+         '..O..',
+         '..O..',
+         '..O..',
+         '..O..']
+    ],
+    [
+        ['.....',
+         '.....',
+         '..O..',
+         '.OOO.',
+         '.....'],
+        ['.....',
+         '..O..',
+         '.OO..',
+         '..O..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.OOO.',
+         '..O..',
+         '.....'],
+        ['.....',
+         '..O..',
+         '..OO.',
+         '..O..',
+         '.....']
+    ],
+    [
+        [
+         '.....',
+         '.....',
+         '..OO.',
+         '.OO..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.OO..',
+         '..OO.',
+         '.....'],
+        ['.....',
+         '.O...',
+         '.OO..',
+         '..O..',
+         '.....'],
+        ['.....',
+         '..O..',
+         '.OO..',
+         '.O...',
+         '.....']
+    ],
+    [
+        ['.....',
+         '..O..',
+         '..O.',
+         '..OO.',
+         '.....'],
+        ['.....',
+         '...O.',
+         '.OOO.',
+         '.....',
+         '.....'],
+        ['.....',
+         '.OO..',
+         '..O..',
+         '..O..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.OOO.',
+         '.O...',
+         '.....']
+    ],
+]
+
+class Tetromino:
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = random.choice(COLORS) # You can choose different colors for each shape
+        self.rotation = 0
